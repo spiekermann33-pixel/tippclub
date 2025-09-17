779899b20ce8f7c887246a811640ec2011f54a5e
spiekermann33@gmail.com <spiekermann33@gmail.com>
2025-09-17 20:33:07 +0000

    Edited settings, Form1 and Form1.RowTemplate4

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -28,17 +28,37 @@ class Form1(Form1Template):
 
     user_tips = []
     for user in app_tables.users.search(active=True):
-      output_row = {"name" : user["user_name"]}
+      output_row = {"name" : user["user_name"],
+                    "user": user, }
       tip = app_tables.tips.get(user=user, gameday=topmatch)
+      output_row.update({"tip_row" : tip})
       if tip:
         output_row.update({
           "tip":f"{tip['home_score']} : {tip['away_score']}"
         })
-
+      
       user_tips.append(output_row)
     self.repeating_panel_user_tips.items = user_tips
 
   def drop_down_gameday_change(self, **event_args):
     """This method is called when an item is selected"""
     self._set_data_grid_user_tips(int(self.drop_down_gameday.selected_value))
-      
+
+  def button_save_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    gameday = app_tables.top_matches.get(season="2025/2026",gameday=int(self.drop_down_gameday.selected_value))
+    for item in self.repeating_panel_user_tips.items:
+      user = item['user']
+      tip_text = item.get("tip","")
+
+      try:
+        home_score, away_score = [int(x) for x in tip_text.split(":")]
+      except Exception:
+        home_score, away_score = None, None  # ungültig oder leer      
+
+      user_tip = app_tables.tips.get(gameday=gameday, user=user)
+      # wenn user bereits tipp abgegeben hat
+      if user_tip:
+        user_tip.update(home_score=home_score,away_score=away_score)
+      elif home_score and away_score:
+        app_tables.tips.add_row(gameday=gameday,user=user,home_score=home_score,away_score=away_score)
