1cbabcfba38a819bf95b59c0027ebdb56eef6c95
spiekermann33@gmail.com <spiekermann33@gmail.com>
2025-09-17 19:09:38 +0000

    Created Form1.RowTemplate3 and Form1.RowTemplate4; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,14 +12,33 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
     # fill drop_down_gameday
+    self._set_drop_down_gameday()
+
+    # fill user tips for gameday
+    self._set_data_grid_user_tips(int(self.drop_down_gameday.selected_value))
+
+  def _set_drop_down_gameday(self, ):
     game_days = []
     for row in app_tables.top_matches.search():
       game_days.append(str(row["gameday"]))
     self.drop_down_gameday.items = game_days
 
-    # fill user tips for gameday
-    topmatch = app_tables.top_matches.get(gameday=3)
+  def _set_data_grid_user_tips(self, gameday):
+    topmatch = app_tables.top_matches.get(gameday=gameday)
+
     user_tips = []
-    for row in app_tables.tips.search(gameday=topmatch):
-      user_tips.append({"tip" : str(row["home_score"]) + str(row["away_score"])})
+    for user in app_tables.users.search(active=True):
+      output_row = {"name" : user["user_name"]}
+      tip = app_tables.tips.get(user=user, gameday=topmatch)
+      if tip:
+        output_row.update({
+          "tip":f"{tip['home_score']} : {tip['away_score']}"
+        })
+
+      user_tips.append(output_row)
     self.repeating_panel_user_tips.items = user_tips
+
+  def drop_down_gameday_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self._set_data_grid_user_tips(int(self.drop_down_gameday.selected_value))
+      
