ce5bd7ef576126ebf60e47ce3a29a6da83cdaafd
spiekermann33@gmail.com <spiekermann33@gmail.com>
2025-10-15 20:06:17 +0000

    Edited Form1.add_new_match

--- client_code/Form1/add_new_match/__init__.py
+++ client_code/Form1/add_new_match/__init__.py
@@ -11,8 +11,30 @@ class add_new_match(add_new_matchTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # fill drop_down_gameday
+    self._set_drop_down_gameday()
 
   def add_new_tips_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Form1')
+
+
+  def _set_drop_down_gameday(self):
+    """Set the drop-down menu items for the game days."""
+    game_days = []
+    # Retrieve all game days from the database
+    for row in app_tables.top_matches.search():
+      game_days.append(str(row["gameday"]))
+    game_days.sort(reverse=True)
+    # Generate a list of all possible game days (1 to 34)
+    all_game_days = [str(i) for i in range(1, 35)]
+    self.drop_down_gameday.items = all_game_days
+    # Determine the highest existing game day
+    if game_days:
+      highest_existing_gameday = int(game_days[0])
+    else:
+      highest_existing_gameday = 0
+    # Set the selected item to the highest existing game day + 1
+    selected_gameday = str(min(highest_existing_gameday + 1, 34))
+    self.drop_down_gameday.selected_value = selected_gameday
+
