697856fcd98a30d3c55c57f36fb09bc9ca20ee82
spiekermann33@gmail.com <spiekermann33@gmail.com>
2025-10-19 19:56:29 +0000

    Edited settings and Form1.add_new_match

--- client_code/Form1/add_new_match/__init__.py
+++ client_code/Form1/add_new_match/__init__.py
@@ -38,17 +38,46 @@ class add_new_match(add_new_matchTemplate):
     selected_gameday = str(min(highest_existing_gameday + 1, 34))
     self.drop_down_gameday.selected_value = selected_gameday
 
+  def get_gameday(self, ):
+    gameday = int(self.drop_down_gameday.selected_value)
+    return app_tables.top_matches.get(gameday=gameday)
+    
   def button_save_click(self, **event_args):
     """This method is called when the button is clicked"""
-    gameday = int(self.drop_down_gameday.selected_value)
+    
     home_team = self.text_box_hometeam.text
     away_team = self.text_box_awayteam.text
 
     home_goals = self.text_box_home_goals.text
-    home_goals = self.text_box_away_goals_goals.text
-    gameday = app_tables.top_matches.get(gameday=gameday)
+    away_goals = self.text_box_away_goals.text
+    
+    gameday = self.get_gameday()
     if gameday is not None:
-      pass
+      # update of existing game
+      gameday.update(home_team=home_team,away_team=away_team,\
+                     home_score=home_goals,away_score=away_goals)
+    else:
+      gameday = int(self.drop_down_gameday.selected_value)
+      jackpot = self.text_box_jackpot.text
+      if home_team is not None and away_team is not None and jackpot is not None:
+        season = self.label_season.text
+        app_tables.top_matches.add_row(gameday=gameday,season=season,home_team=home_team,\
+                                    away_team=away_team,home_score=home_goals,\
+                                      away_score=away_goals, jackpot=jackpot)
+        
+  def drop_down_gameday_change(self, **event_args):
+    """This method is called when an item is selected"""
+    gameday = self.get_gameday()
+    if gameday is not None:
+      self.text_box_hometeam.text = gameday["home_team"]
+      self.text_box_awayteam.text = gameday["away_team"]
+      self.text_box_home_goals.text = gameday["home_score"]
+      self.text_box_away_goals.text = gameday["away_score"]
+    else:
+      self.text_box_hometeam.text = ""
+      self.text_box_awayteam.text = ""
+      self.text_box_home_goals.text = ""
+      self.text_box_away_goals.text = ""
 
     
 
